90b470e65662706edc4679d92bac8291e97f1c8e
Karinigi-Shivakumar <shivakumarkaringi@gmail.com>
2024-12-11 14:59:58 +0530

    feat: create instance terraform script

---
+++ terraform/terraform-scripts/first_instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "first-instance" {
+    ami = "ami-0453ec754f44f9a4a"
+    instance_type = "t2.micro"
+    key_name = "ci-vprofile-key"
+    vpc_security_group_ids = "sg-0e29eecde75c7e285"
+    tags = {
+      Name = "project1-instance"
+    }
+}
